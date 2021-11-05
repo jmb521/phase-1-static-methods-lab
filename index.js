fb2a385edbd77a4aa0809a8ec4269396916dbae7
Jennifer Pazos <jmp521@gmail.com>
2021-11-05 18:29:39 -0500

    commit tree a34a9d878be43cb89d495832ed9873f7eada6334

--- index.js
+++ index.js
@@ -1,3 +1,35 @@
 class Formatter {
-  //add static methods here
+  
+  static capitalize(word) {
+		return word[0].toUpperCase() + word.slice(1)
+	}
+
+  static sanitize(sentence) {
+    return sentence.split("").map(s => {
+      
+      return (/([a-zA-Z]|\s|-|')/.test(s)) ? s : ""
+
+
+    }).join("")
+
+  }
+
+  static titleize(sentence) {
+    const arr = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by','from']
+    
+    let modSentence = sentence.split(" ").map(s => {
+      if(!arr.includes(s)) {
+        return Formatter.capitalize(s)
+      }
+      // } else {
+      //   return s
+      // }
+      
+   }).join(" ")
+   return Formatter.capitalize(modSentence)
+   
+  }
+
+  
 }
+
